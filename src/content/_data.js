b1196ed7fcfe4c7c6ad83c1e2fc341478e61b02e
Zygar <a@andywilkinson.co.nz>
2018-02-09 13:36:57 +1300

    first cut at an answer structure

--- src/content/_data.js
+++ src/content/_data.js
@@ -1,3 +1,9 @@
 export default {
-    // JSON style data here. 
+    questions: [
+        {
+            question: "What is the question",
+            answerIndices: [1, 3]
+        }
+    ],
+    answers: ["One", "Two", "Three", "Four", "Five"]
 }
