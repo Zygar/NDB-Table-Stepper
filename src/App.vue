d8828f517cdf65f850850a2439323301d74fe47c
Zygar <a@andywilkinson.co.nz>
2018-02-09 13:12:42 +1300

    Resets to pretty much a blank vue.js instance, leaving the old components for reference

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,19 @@
 <template>
     <main id="App"  class="component">
-        <NavigationView  :tree="allItems" :selected="selected" />
-        <InfoView :selected="selected" />
+       <h1>Welcome</h1>
     </main>
 </template>
 
 <script>
-import sourceData from './content/_data.js';
-import InfoView from './components/InfoView.vue';
-import NavigationView from './components/NavigationView.vue';
+// import sourceData from './content/_data.js';
+// import InfoView from './components/InfoView.vue';
+// import NavigationView from './components/NavigationView.vue';
 import eventHub from './eventHub.js';
 
 export default {
     name: 'app', 
-    components: { InfoView, NavigationView },
-    data () {
-        let current = {
-            callNext: "",
-            allItems: sourceData,
-            selected: {}
-        }
-        return current;
-    }, 
-    methods: {
-        updateInfo: function(e) {
-            this.selectCurrent(this.callNext);
-        },
-        newSelectCurrent: function(newContent) {
-            this.selected = newContent;
-        }
-    },
-    mounted () {
-        this.newSelectCurrent(this.allItems);
-        eventHub.$on('navigationRequest', (requestedChild) => {
-            this.newSelectCurrent(requestedChild);
-
-            // requestedChild.isActive = true;
-            console.log(requestedChild)
-        });
-        // eventHub.$on('navigationRequest', function(requestedChild) {
-        //     console.log("Navigation request FIRED", requestedChild);
-        //     console.log('mounted scope is', this);
-
-        //     // this.selectCurrent(requestedChild);
-        // })
-    }
+    components: {  },
+    data () { }, 
+    methods: { }
 }
 </script>
--- src/content/_data.js
+++ src/content/_data.js
@@ -1,22 +1,3 @@
-import oral from './oral/_data.js'
-import parenteral from './parenteral/_data.js'
-import inhaled from './inhaled/_data.js'
-import eyeFormulations from './eye-formulations/_data.js'
-import earFormulations from './ear-formulations/_data.js'
-import nasalFormulations from './nasal-formulations/_data.js'
-import rectalVaginal from './rectal-vaginal/_data.js'
-import topical from './topical/_data.js'
-
 export default {
-    name: "Dose forms",
-    children: [
-        oral,
-        parenteral,
-        inhaled,
-        eyeFormulations,
-        earFormulations,
-        nasalFormulations,
-        rectalVaginal,
-        topical
-    ]
+    // JSON style data here. 
 }
